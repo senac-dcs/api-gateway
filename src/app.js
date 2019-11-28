fc9ca650801fbe59c5d178e41449bcde70a82813
Lucas Lacerda <lucasalvlacerda@gmail.com>
2019-11-27 21:05:13 -0300

    add dotenv config

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 const http = require('http');
 const express = require('express');
-const httProxy = require('express-http-proxy')
+const httpProxy = require('express-http-proxy')
 const app = express();
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+require('dotenv').config()
 
-const alunoServiceProxy = httpProxy('http://localhost:3001');
-const professoresServiceProxy = httpProxy('http://localhost:3002');
+const professoresServiceProxy = httpProxy(`${process.env.URL_PROFESSORES}:${process.env.PORT_PROFESSORES}`);
+const alunosServiceProxy = httpProxy(`${process.env.URL_ALUNOS}:${process.env.PORT_ALUNOS}`);
 
 app.get('/professores', (req, res, next) => {
     professoresServiceProxy(req, res, next);
 })
+
+app.get('/alunos', (req, res, next) => {
+    alunosServiceProxy(req, res, next);
+})
+
+console.log(process.env.PORT_ALUNOS);
 
 app.use(logger('dev'));
 app.use(helmet());
@@ -20,4 +27,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 const server = http.createServer(app);
-server.listen(env.process.PORT)
+server.listen(process.env.GATEWAY_PORT, () => {
+    console.log(`listem to port ${process.env.GATEWAY_PORT}`)
+})
+
+app.get('/', (req, res, next) => {
+    res.send(`welcome to /`);
+})
